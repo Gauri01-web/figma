ab109ca84028e0cfd5457cd356d12c29765df238
Gauri <sabbalwargauri@gmail.com>
2025-04-07 15:33:22 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-[#191A23] text-white p-6 text-center">
+      <p>© 2023 Positivus. All Rights Reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
